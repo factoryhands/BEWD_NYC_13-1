003cec7f46031231c7e25aa7162afbefc0e66763
Jennifer Spriggs <jenniferlspriggs@gmail.com>
2014-10-15 21:41:52 -0400

    Apartment homework assignment

--- jennifer_spriggs/apartment/lib/apartment.rb
+++ jennifer_spriggs/apartment/lib/apartment.rb
@@ -1,2 +1,14 @@
 class Apartment
+  
+  attr_accessor :apartment_number, :apartment_square_feet, :apartment_bedrooms, :apartment_bathrooms, :number, :renter, :rent
+
+  def initialize (apartment_number, apartment_square_feet, apartment_bedrooms, apartment_bathrooms)
+     @apartment_number = apartment_number
+     @apartment_square_feet = apartment_square_feet
+     @apartment_bedrooms = apartment_bedrooms
+     @apartment_bathrooms = apartment_bathrooms
+     @number =@apartment_number
+    
+    @renter =[]
+  end
 end
--- jennifer_spriggs/apartment/lib/building.rb
+++ jennifer_spriggs/apartment/lib/building.rb
@@ -1,2 +1,19 @@
 class Building
+  attr_accessor :building_name, :building_address, :apartments
+
+
+  def initialize(building_name, building_address)
+    @building_name = building_name
+    @building_address = building_address
+    @apartments=[]
+  end
+
+  def view_apartments
+    @apartments.each do |apt|
+      puts "Number: #{apartment.number}" 
+      puts "Rent: #{apartment.rent}"
+      puts "Square Feet: #{apartment.square_feet}"
+    end
+  end
 end
+
